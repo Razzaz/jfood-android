bfaecc14fdd50359225402f86b7abbcd4ca90584
Ridho Gani <ridhoadhadigani@gmail.com>
2020-04-25 09:58:14 +0700

    modul 9 tp

--- app/src/main/java/com/example/jfood_android/RegisterActivity.java
+++ app/src/main/java/com/example/jfood_android/RegisterActivity.java
@@ -1,14 +1,11 @@
 package com.example.jfood_android;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
